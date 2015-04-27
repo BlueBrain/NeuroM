f1290ee02dbe6ea4cd1d68b0ff90e93a71d3cb77
Lida Kanari <lida.kanari@epfl.ch>
2015-04-27 15:39:29 +0200

    Changes to resolve pylint, pep8 errors and clean up view.common code
    Change-Id: I0f0192f6e5fda70266164092866371459e29bdaf

--- neurom/view/common.py
+++ neurom/view/common.py
@@ -180,8 +180,10 @@ def save_plot(fig, **kwargs):
 
     print 'Plot Saved:', output
 
+    return fig
 
-def style_plot(fig, ax, **kwargs):
+
+def plot_style(fig, ax, **kwargs):
 
     """
     Function to set the basic options of a matplotlib figure,
@@ -195,11 +197,133 @@ def style_plot(fig, ax, **kwargs):
     -------
     pretitle : str
         String to include before the general title of the figure.
-        Default is None.
+        Default value is None.
 
     posttitle : str
         String to include after the general title of the figure.
-        Default is None.
+        Default value is None.
+
+    title : str
+        The title for the figure
+        Default value is "Figure".
+
+    no_title : boolean
+        Defines the presence of a title in the figure.
+        If True the title will be set to an empty string "".
+        Default value is False.
+
+    title_fontsize : int
+        Defines the size of the title's font.
+        Default value is 14.
+
+    title_arg : dict
+        Defines the arguments that will be passsed
+        into matplotlib as title arguments.
+        Default value is None.
+
+    xlabel : str
+        The xlabel for the figure
+        Default value is "X".
+
+    ylabel : str
+        The xlabel for the figure
+        Default value is "Y".
+
+    no_labels : boolean
+        Defines the presence of the labels in the figure.
+        If True the labels will be set to an empty string "".
+        Default value is False.
+
+    no_xlabel : boolean
+        Defines the presence of the xlabel in the figure.
+        If True the xlabel will be set to an empty string "".
+        Default value is False.
+
+    no_ylabel : boolean
+        Defines the presence of the ylabel in the figure.
+        If True the ylabel will be set to an empty string "".
+        Default value is False.
+
+    label_fontsize : int
+        Defines the size of the labels' font.
+        Default value is 14.
+
+    xlabel_arg : dict
+        Defines the arguments that will be passsed
+        into matplotlib as xlabel arguments.
+        Default value is None.
+
+    ylabel_arg : dict
+        Defines the arguments that will be passsed
+        into matplotlib as ylabel arguments.
+        Default value is None.
+
+    no_ticks : boolean
+        Defines the presence of x-y ticks in the figure.
+        If True the ticks will be empty.
+        Default value is False.
+
+    no_xticks : boolean
+        Defines the presence of x ticks in the figure.
+        If True the ticks will be empty.
+        Default value is False.
+
+    no_yticks : boolean
+        Defines the presence of y ticks in the figure.
+        If True the ticks will be empty.
+        Default value is False.
+
+    xticks : list of ticks
+        Defines the values of x ticks in the figure.
+        Default value is None.
+
+    yticks : list of ticks
+        Defines the values of y ticks in the figure.
+        Default value is None.
+
+    tick_fontsize : int
+        Defines the size of the ticks' font.
+        Default value is 12.
+
+    xticks_arg : dict
+        Defines the arguments that will be passsed
+        into matplotlib as xticks arguments.
+        Default value is None.
+
+    yticks_arg : dict
+        Defines the arguments that will be passsed
+        into matplotlib as yticks arguments.
+        Default value is None.
+
+    no_limits : boolean
+        Defines the presence of plot limits in the figure.
+        Default value is False.
+
+    no_xlim : boolean
+        Defines the presence of plot x-limits in the figure.
+        Default value is False.
+
+    no_ylim : boolean
+        Defines the presence of plot y-limits in the figure.
+        Default value is False.
+
+    xlim: list of two floats
+        Defines the min and the max values in x-axis.
+        Default in None
+
+    ylim: list of two floats
+        Defines the min and the max values in y-axis.
+        Default in None
+
+    no_legend : boolean
+        Defines the presence of a legend in the figure.
+        If True the legend will not be included in the Figure.
+        Default value is True.
+
+    legend_arg : dict
+        Defines the arguments that will be passsed
+        into matplotlib as legend arguments.
+        Default value is None.
 
     prefile : str
         String to include before the general filename of the figure.
@@ -229,72 +353,28 @@ def style_plot(fig, ax, **kwargs):
         If True the saved figure will have a transparent background.
         Default value is False.
 
-    title : str
-        The title for the figure
-        Default is "Figure".
-
-    xlabel : str
-        Label for the x-axis.
-        Default is "X".
-
-    ylabel : str
-        Label for the y-axis.
-        Default is "Y".
-
-    show_plot : bool
+    show_plot : boolean
         If True the figure is displayed.
         Default value is True.
 
-    titlefontsize : int
-        The fontsize for the title. Default is 16.
+    no_axes: boolean
+        If True the labels and the frame will be set off.
+        Default value is False.
 
-    labelfontsize : int
-        The fontsize for the labels. Default is 14,
+    tight: boolean
+        If True the set layout of matplotlib will be activated.
+        Default value is False.
 
     Returns
     -------
-    figure_output : figure
+    figure_output : figure, axes
 
     """
-
-    # Definition of title options
-    no_title = kwargs.get('no_title', False)
+    # Definition of title/file naming variables
+    prefile = kwargs.get('prefile', '')
+    postfile = kwargs.get('postfile', '')
     pretitle = kwargs.get('pretitle', '')
     posttitle = kwargs.get('posttitle', '')
-    title = kwargs.get('title', 'Figure')
-    titlefontsize = kwargs.get('titlefontsize', 14)
-    titlearg = kwargs.get('titlearg', {})
-
-    # Definition of label options
-    no_labels = kwargs.get('no_labels', False)
-    no_xlabel = kwargs.get('no_xlabel', False)
-    no_ylabel = kwargs.get('no_ylabel', False)
-    xlabel = kwargs.get('xlabel', 'X')
-    ylabel = kwargs.get('ylabel', 'Y')
-    labelfontsize = kwargs.get('labelfontsize', 14)
-    xlabelarg = kwargs.get('xlabelarg', {})
-    ylabelarg = kwargs.get('ylabelarg', {})
-
-    # Definition of tick options
-    no_ticks = kwargs.get('no_ticks', False)
-    no_xticks = kwargs.get('no_xticks', False)
-    no_yticks = kwargs.get('no_yticks', False)
-    x_ticks = kwargs.get('x_ticks', [])
-    y_ticks = kwargs.get('y_ticks', [])
-    tickfontsize = kwargs.get('tickfontsize', 12)
-    xticksarg = kwargs.get('xticksarg', {})
-    yticksarg = kwargs.get('yticksarg', {})
-
-    # Definition of legend options
-    no_legend = kwargs.get('no_legend', True)
-    legendarg = kwargs.get('legendarg', {})
-
-    # Definition of limit options
-    no_limits = kwargs.get('no_limits', False)
-    no_xlim = kwargs.get('no_xlim', False)
-    no_ylim = kwargs.get('no_ylim', False)
-    xlim = kwargs.get('xlim', None)
-    ylim = kwargs.get('ylim', None)
 
     # Definition of global options
     no_axes = kwargs.get('no_axes', False)
@@ -302,51 +382,19 @@ def style_plot(fig, ax, **kwargs):
     tight = kwargs.get('tight', False)
 
     # Definition of save options
-    prefile = kwargs.get('prefile', '')
-    postfile = kwargs.get('postfile', '')
     output_path = kwargs.get('output_path', None)
-    output_name = kwargs.get('output_name', 'Figure')
-    output_format = kwargs.get('output_format', 'png')
-    dpi = kwargs.get('dpi', 300)
-    transparent = kwargs.get('transparent', False)
 
     pretitle, posttitle, prefile, postfile = figure_naming(pretitle, posttitle, prefile, postfile)
 
-    if no_title:
-        ax.set_title('')
-    else:
-        ax.set_title(pretitle + title + posttitle, fontsize=titlefontsize, **titlearg)
+    fig, ax = plot_title(fig, ax, **kwargs)
 
-    if no_labels or no_xlabel:
-        ax.set_xlabel('')
-    else:
-        ax.set_xlabel(xlabel, fontsize=labelfontsize, **xlabelarg)
+    fig, ax = plot_labels(fig, ax, **kwargs)
 
-    if no_labels or no_ylabel:
-        ax.set_ylabel('')
-    else:
-        ax.set_ylabel(ylabel, fontsize=labelfontsize, **ylabelarg)
+    fig, ax = plot_ticks(fig, ax, **kwargs)
 
-    if no_ticks or no_xticks:
-        ax.xaxis.set_ticks([])
-    else:
-        ax.xaxis.set_ticklabels(x_ticks)
-        ax.xaxis.set_tick_params(labelsize=tickfontsize, **xticksarg)
+    fig, ax = plot_limits(fig, ax, **kwargs)
 
-    if no_ticks or no_yticks:
-        ax.yaxis.set_ticks([])
-    else:
-        ax.yaxis.set_ticklabels(y_ticks)
-        ax.yaxis.set_tick_params(labelsize=tickfontsize, **yticksarg)
-
-    if not no_limits and not no_xlim:
-        ax.set_xlim(xlim)
-
-    if not no_limits and not no_ylim:
-        ax.set_ylim(ylim)
-
-    if not no_legend:
-        ax.legend(**legendarg)
+    fig, ax = plot_legend(fig, ax, **kwargs)
 
     if no_axes:
         ax.set_frame_on(False)
@@ -354,21 +402,362 @@ def style_plot(fig, ax, **kwargs):
         ax.yaxis.set_visible(False)
 
     if tight:
-        plt.gcf().set_tight_layout(True)
-
-    save_params = {'prefile': prefile,
-                   'postfile': postfile,
-                   'output_path': output_path,
-                   'output_name': output_name,
-                   'output_format': output_format,
-                   'dpi': dpi,
-                   'transparent': transparent}
+        fig.set_tight_layout(True)
 
     if output_path is not None:
-        save_plot(fig=ax, **save_params)
+        fig = save_plot(fig=ax, **kwargs)
 
     if not show_plot:
         plt.close()
         return (None, None)
+
+    return fig, ax
+
+
+def plot_title(fig, ax, **kwargs):
+
+    """
+    Function that defines the title options
+    of a matplotlib plot.
+
+    Parameters
+    ----------
+    fig: matplotlib figure
+
+    ax: matplotlib axes
+
+    Options
+    -------
+    pretitle : str
+        String to include before the general title of the figure.
+        Default value is None.
+
+    posttitle : str
+        String to include after the general title of the figure.
+        Default value is None.
+
+    title : str
+        The title for the figure
+        Default value is "Figure".
+
+    no_title : boolean
+        Defines the presence of a title in the figure.
+        If True the title will be set to an empty string "".
+        Default value is False.
+
+    title_fontsize : int
+        Defines the size of the title's font.
+        Default value is 14.
+
+    title_arg : dict
+        Defines the arguments that will be passsed
+        into matplotlib as title arguments.
+        Default value is None.
+
+    Returns
+    -------
+    figure_output : figure, axes
+
+    """
+
+    # Definition of title options
+    pretitle = kwargs.get('pretitle', '')
+    posttitle = kwargs.get('posttitle', '')
+    title = kwargs.get('title', 'Figure')
+    no_title = kwargs.get('no_title', False)
+    title_fontsize = kwargs.get('titlefontsize', 14)
+    title_arg = kwargs.get('titlearg', None)
+
+    if title_arg is None:
+        title_arg = {}
+
+    if no_title:
+        ax.set_title('')
+    else:
+        ax.set_title(pretitle + title + posttitle,
+                     fontsize=title_fontsize, **title_arg)
+
+    return fig, ax
+
+
+def plot_labels(fig, ax, **kwargs):
+
+    """
+    Function that defines the labels options
+    of a matplotlib plot.
+
+    Parameters
+    ----------
+    fig: matplotlib figure
+
+    ax: matplotlib axes
+
+    Options
+    -------
+    xlabel : str
+        The xlabel for the figure
+        Default value is "X".
+
+    ylabel : str
+        The xlabel for the figure
+        Default value is "Y".
+
+    no_labels : boolean
+        Defines the presence of the labels in the figure.
+        If True the labels will be set to an empty string "".
+        Default value is False.
+
+    no_xlabel : boolean
+        Defines the presence of the xlabel in the figure.
+        If True the xlabel will be set to an empty string "".
+        Default value is False.
+
+    no_ylabel : boolean
+        Defines the presence of the ylabel in the figure.
+        If True the ylabel will be set to an empty string "".
+        Default value is False.
+
+    label_fontsize : int
+        Defines the size of the labels' font.
+        Default value is 14.
+
+    xlabel_arg : dict
+        Defines the arguments that will be passsed
+        into matplotlib as xlabel arguments.
+        Default value is None.
+
+    ylabel_arg : dict
+        Defines the arguments that will be passsed
+        into matplotlib as ylabel arguments.
+        Default value is None.
+
+    Returns
+    -------
+    figure_output : figure, axes
+
+    """
+
+    # Definition of label options
+    xlabel = kwargs.get('xlabel', 'X')
+    ylabel = kwargs.get('ylabel', 'Y')
+    no_labels = kwargs.get('no_labels', False)
+    no_xlabel = kwargs.get('no_xlabel', False)
+    no_ylabel = kwargs.get('no_ylabel', False)
+    label_fontsize = kwargs.get('labelfontsize', 14)
+    xlabel_arg = kwargs.get('xlabelarg', None)
+    ylabel_arg = kwargs.get('ylabelarg', None)
+
+    if xlabel_arg is None:
+        xlabel_arg = {}
+
+    if ylabel_arg is None:
+        ylabel_arg = {}
+
+    if no_labels:
+        ax.set_xlabel('')
+        ax.set_ylabel('')
+    else:
+        if no_xlabel:
+            ax.set_xlabel('')
+        else:
+            ax.set_xlabel(xlabel,
+                          fontsize=label_fontsize,
+                          **xlabel_arg)
+        if no_ylabel:
+            ax.set_ylabel('')
+        else:
+            ax.set_ylabel(ylabel,
+                          fontsize=label_fontsize,
+                          **ylabel_arg)
+
+    return fig, ax
+
+
+def plot_ticks(fig, ax, **kwargs):
+
+    """
+    Function that defines the labels options
+    of a matplotlib plot.
+
+    Parameters
+    ----------
+    fig: matplotlib figure
+
+    ax: matplotlib axes
+
+    Options
+    -------
+    no_ticks : boolean
+        Defines the presence of x-y ticks in the figure.
+        If True the ticks will be empty.
+        Default value is False.
+
+    no_xticks : boolean
+        Defines the presence of x ticks in the figure.
+        If True the ticks will be empty.
+        Default value is False.
+
+    no_yticks : boolean
+        Defines the presence of y ticks in the figure.
+        If True the ticks will be empty.
+        Default value is False.
+
+    xticks : list of ticks
+        Defines the values of x ticks in the figure.
+        Default value is None.
+
+    yticks : list of ticks
+        Defines the values of y ticks in the figure.
+        Default value is None.
+
+    tick_fontsize : int
+        Defines the size of the ticks' font.
+        Default value is 12.
+
+    xticks_arg : dict
+        Defines the arguments that will be passsed
+        into matplotlib as xticks arguments.
+        Default value is None.
+
+    yticks_arg : dict
+        Defines the arguments that will be passsed
+        into matplotlib as yticks arguments.
+        Default value is None.
+
+    Returns
+    -------
+    figure_output : figure, axes
+
+    """
+
+    # Definition of tick options
+    no_ticks = kwargs.get('no_ticks', False)
+    no_xticks = kwargs.get('no_xticks', False)
+    no_yticks = kwargs.get('no_yticks', False)
+    xticks = kwargs.get('xticks', None)
+    yticks = kwargs.get('yticks', None)
+    tick_fontsize = kwargs.get('tickfontsize', 12)
+    xticks_arg = kwargs.get('xticksarg', None)
+    yticks_arg = kwargs.get('yticksarg', None)
+
+    if xticks_arg is None:
+        xticks_arg = {}
+
+    if yticks_arg is None:
+        yticks_arg = {}
+
+    if not no_ticks:
+        if xticks is not None:
+            if not no_xticks:
+                ax.set_xticks(xticks)
+                ax.xaxis.set_tick_params(labelsize=tick_fontsize, **xticks_arg)
+            else:
+                ax.xaxis.set_ticks([])
+        if yticks is not None:
+            if not no_yticks:
+                ax.set_yticks(yticks)
+                ax.yaxis.set_tick_params(labelsize=tick_fontsize, **xticks_arg)
+            else:
+                ax.yaxis.set_ticks([])
+    else:
+        ax.xaxis.set_ticks([])
+        ax.yaxis.set_ticks([])
+
+    return fig, ax
+
+
+def plot_limits(fig, ax, **kwargs):
+
+    """
+    Function that defines the limit options
+    of a matplotlib plot.
+
+    Parameters
+    ----------
+    fig: matplotlib figure
+
+    ax: matplotlib axes
+
+    Options
+    -------
+    no_limits : boolean
+        Defines the presence of plot limits in the figure.
+        Default value is False.
+
+    no_xlim : boolean
+        Defines the presence of plot x-limits in the figure.
+        Default value is False.
+
+    no_ylim : boolean
+        Defines the presence of plot y-limits in the figure.
+        Default value is False.
+
+    xlim: list of two floats
+        Defines the min and the max values in x-axis.
+        Default in None
+
+    ylim: list of two floats
+        Defines the min and the max values in y-axis.
+        Default in None
+
+    Returns
+    -------
+    figure_output : figure, axes
+
+    """
+    # Definition of limit options
+    no_limits = kwargs.get('no_limits', False)
+    no_xlim = kwargs.get('no_xlim', False)
+    no_ylim = kwargs.get('no_ylim', False)
+    xlim = kwargs.get('xlim', None)
+    ylim = kwargs.get('ylim', None)
+
+    if not no_limits:
+        if not no_xlim:
+            ax.set_xlim(xlim)
+        if not no_ylim:
+            ax.set_ylim(ylim)
+
+    return fig, ax
+
+
+def plot_legend(fig, ax, **kwargs):
+
+    """
+    Function that defines the legend options
+    of a matplotlib plot.
+
+    Parameters
+    ----------
+    fig: matplotlib figure
+
+    ax: matplotlib axes
+
+    Options
+    -------
+    no_legend : boolean
+        Defines the presence of a legend in the figure.
+        If True the legend will not be included in the Figure.
+        Default value is True.
+
+    legend_arg : dict
+        Defines the arguments that will be passsed
+        into matplotlib as legend arguments.
+        Default value is None.
+
+    Returns
+    -------
+    figure_output : figure, axes
+
+    """
+    # Definition of legend options
+    no_legend = kwargs.get('no_legend', True)
+    legend_arg = kwargs.get('legendarg', None)
+
+    if legend_arg is None:
+        legend_arg = {}
+
+    if not no_legend:
+        ax.legend(**legend_arg)
 
     return fig, ax
--- neurom/view/tests/test_common.py
+++ neurom/view/tests/test_common.py
@@ -3,8 +3,18 @@ from neurom.view.common import plt
 from neurom.view.common import figure_naming
 from neurom.view.common import get_figure
 from neurom.view.common import save_plot
-from neurom.view.common import style_plot
+from neurom.view.common import plot_style
+from neurom.view.common import plot_title
+from neurom.view.common import plot_labels
+from neurom.view.common import plot_legend
+from neurom.view.common import plot_limits
+from neurom.view.common import plot_ticks
+
 import os
+import numpy as np
+
+fig_name = 'Figure.png'
+fig_dir  = './Testing_save_in_directory/'
 
 def test_figure_naming():
     pretitle, posttitle, prefile, postfile = figure_naming(pretitle='Test', posttitle=None, prefile="", postfile=3)
@@ -36,60 +46,102 @@ def test_get_figure():
     plt.close('all')
 
 def test_save_plot():
-    fig_name = 'Figure.png'
-    fig_dir  = './Testing_save_in_directory/'
     if os.path.isfile(fig_name):
         os.remove(fig_name)
     fig_old = plt.figure()
-    save_plot(fig_old)
+    fig = save_plot(fig_old)
     nt.ok_(os.path.isfile(fig_name)==True)
     os.remove(fig_name)
     if os.path.isdir(fig_dir):
         for data in os.listdir(fig_dir):
             os.remove(fig_dir + data)
         os.rmdir(fig_dir)
-    save_plot(fig_old, output_path=fig_dir)
+    fig = save_plot(fig_old, output_path=fig_dir)
     nt.ok_(os.path.isfile(fig_dir + fig_name)==True)
     os.remove(fig_dir + fig_name)
     os.rmdir(fig_dir)
     plt.close('all')
 
-def test_style_plot():
-    fig_name = 'Figure.png'
-    fig_dir  = './Testing_save_in_directory/'
-    fig = plt.figure()
-    ax  = fig.add_subplot(111)
-    fig0, ax0 = style_plot(fig, ax, title='Test', xlabel='T')
-    nt.ok_(ax0.get_title() == 'Test')
-    nt.ok_(ax0.get_xlabel() == 'T')
-    nt.ok_(ax0.get_ylabel() == 'Y')
-    nt.ok_(ax0.get_legend() is None)
-    fig1, ax1 = style_plot(fig, ax, no_title=True, no_labels=True, no_ticks=True)
-    nt.ok_(ax1.get_title() == '')
-    nt.ok_(ax1.get_xlabel() == '')
-    nt.ok_(ax1.get_ylabel() == '')
+fig = plt.figure()
+ax  = fig.add_subplot(111)
+ax.plot([0,0], [1,2], label='test')
+xlim = ax.get_xlim()
+ylim = ax.get_ylim()
+
+def test_plot_title():
+    fig1, ax1 = plot_title(fig, ax)
+    nt.ok_(ax1.get_title() == 'Figure')
+    fig1, ax1 = plot_title(fig, ax, title='Test')
+    nt.ok_(ax1.get_title() == 'Test')
+    fig1, ax1 = plot_title(fig, ax, no_title=True)
+    nt.ok_(ax1.get_title() == "")
+    
+def test_plot_labels():
+    fig1, ax1 = plot_labels(fig, ax)
+    nt.ok_(ax1.get_xlabel() == 'X')
+    nt.ok_(ax1.get_ylabel() == 'Y')
+    fig1, ax1 = plot_labels(fig, ax, xlabel='T', no_ylabel=True)
+    nt.ok_(ax1.get_xlabel() == 'T')
+    nt.ok_(ax1.get_ylabel() == "")
+    fig1, ax1 = plot_labels(fig, ax, ylabel='T', no_xlabel=True)
+    nt.ok_(ax1.get_ylabel() == 'T')
+    nt.ok_(ax1.get_xlabel() == "")
+    fig1, ax1 = plot_labels(fig, ax, no_labels=True)
+    nt.ok_(ax1.get_xlabel() == "")
+    nt.ok_(ax1.get_ylabel() == "")
+
+def test_plot_legend():
+    fig1, ax1 = plot_legend(fig, ax)
+    legend = ax1.get_legend()
+    nt.ok_(legend is None)
+    fig1, ax1 = plot_legend(fig, ax, no_legend=False)
+    legend = ax1.get_legend()
+    nt.ok_(legend.get_texts()[0].get_text() == 'test')
+
+def test_plot_limits():
+    fig1, ax1 = plot_limits(fig, ax, no_limits=True)
+    nt.ok_(ax1.get_xlim() == xlim)
+    nt.ok_(ax1.get_ylim() == ylim)
+    fig1, ax1 = plot_limits(fig, ax, xlim=(0,100), ylim=(-100,0))
+    nt.ok_(ax1.get_xlim() == (0,100))
+    nt.ok_(ax1.get_ylim() == (-100,0))
+
+def test_plot_ticks():
+    fig1, ax1 = plot_ticks(fig, ax, no_ticks=False)
+    nt.ok_(len(ax1.get_xticks()) != 0 )
+    nt.ok_(len(ax1.get_yticks()) != 0 )
+    fig1, ax1 = plot_ticks(fig, ax, no_ticks=True)
     nt.ok_(len(ax1.get_xticks()) == 0 )
     nt.ok_(len(ax1.get_yticks()) == 0 )
-    ax.plot([0,0], [1,2], label='test')
-    fig2, ax2 = style_plot(fig, ax, no_legend=False, tight=True, no_axes=True)
-    legend = ax2.get_legend()
-    nt.ok_(legend.get_texts()[0].get_text() == 'test')
-    nt.ok_(fig2.get_tight_layout() == True)
-    nt.ok_(ax2.get_frame_on() == False)
-    nt.ok_(ax2.xaxis.get_visible() == False)
-    nt.ok_(ax2.yaxis.get_visible() == False)
-    plt.close('all')
+    fig1, ax1 = plot_ticks(fig, ax, no_xticks=True, no_yticks=True)
+    nt.ok_(len(ax1.get_xticks()) == 0 )
+    nt.ok_(len(ax1.get_yticks()) == 0 )
+    fig1, ax1 = plot_ticks(fig, ax, xticks=np.arange(3), yticks=np.arange(4))
+    nt.ok_(len(ax1.get_xticks()) == 3 )
+    nt.ok_(len(ax1.get_yticks()) == 4 )
+
+def test_plot_style():
+    fig1, ax1 = plot_style(fig, ax)
+    nt.ok_(ax1.get_title() == 'Figure')
+    nt.ok_(ax1.get_xlabel() == 'X')
+    nt.ok_(ax1.get_ylabel() == 'Y')
+    fig1, ax1 = plot_style(fig, ax, no_axes=True)
+    nt.ok_(ax1.get_frame_on() == False)
+    nt.ok_(ax1.xaxis.get_visible() == False)
+    nt.ok_(ax1.yaxis.get_visible() == False)
+    fig1, ax1 = plot_style(fig, ax, tight=True)
+    nt.ok_(fig1.get_tight_layout() == True)
+    fig1, ax1 = plot_style(fig, ax, show_plot=False)
+    nt.ok_(fig1 is None)
+    nt.ok_(ax1 is None)
     if os.path.isdir(fig_dir):
         for data in os.listdir(fig_dir):
             os.remove(fig_dir + data)
         os.rmdir(fig_dir)
-    fig3, ax3 = style_plot(fig, ax, show_plot=False, output_path=fig_dir, output_name='Figure')
-    nt.ok_(len(plt.get_fignums()) == 0)
+    fig1, ax1 = plot_style(fig, ax, output_path=fig_dir, output_name='Figure')
     nt.ok_(os.path.isfile(fig_dir + fig_name)==True)
     os.remove(fig_dir + fig_name)
     os.rmdir(fig_dir)
-    plt.close('all')
-
 
 
 
