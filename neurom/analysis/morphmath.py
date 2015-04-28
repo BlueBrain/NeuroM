01d51ab7a873adb29991b9d3fd40f80e4cc651a4
Juan Palacios <juan.palacios@epfl.ch>
2015-04-28 15:27:39 +0200

    Change layout of neurom.core.point.Point class and simplyfy maths functions.
    (t, x, y, z, r) -> (x, y, z, r, t)
    
    neurom.analysis.morphmath functions re-implemented in numpy.
    
    Change-Id: I05934cf4c4e25306cb4667dd1067ba6ab976968c

--- neurom/analysis/morphmath.py
+++ neurom/analysis/morphmath.py
@@ -1,48 +1,58 @@
 '''Mathematics functions used to compute morphometrics'''
 
-from math import sqrt
 from math import acos
-from collections import namedtuple
-import itertools
-
-Vector3D = namedtuple('Vector3D', ('x', 'y', 'z'))
+from itertools import combinations
+import numpy as np
 
 
-def point_dist(point1, point2):
-    '''compute the distance between center of two Point'''
-    x = point1.x - point2.x
-    y = point1.y - point2.y
-    z = point1.z - point2.z
-    return sqrt(x * x + y * y + z * z)
+np.seterr(all='raise')  # raise exceptions for floating point errors.
 
 
-def vector(point1, point2):
-    '''compute vecteur between center of two Point '''
-    p = Vector3D(point1.x - point2.x, point1.y - point2.y, point1.z - point2.z)
-    return p
+def point_dist(p1, p2):
+    '''compute the euclidian distance between two 3D points
+
+    Args:
+        p1, p2: indexable objects with
+        indices 0, 1, 2 corresponding to 3D cartesian coordinates.
+    Returns:
+        The euclidian distance between the points.
+    '''
+    return np.linalg.norm(np.subtract(p1[0:3], p2[0:3]))
 
 
-def norm(v3d):
-    ''' compute norm of vector represent by given 3d point'''
-    return sqrt(v3d.x * v3d.x + v3d.y * v3d.y + v3d.z * v3d.z)
+def vector(p1, p2):
+    '''compute vector between two 3D points
+
+    Args:
+        p1, p2: indexable objects with
+        indices 0, 1, 2 corresponding to 3D cartesian coordinates.
+
+    Returns:
+        3-vector from p1 - p2
+    '''
+    return np.subtract(p1[0:3], p2[0:3])
 
 
-def dot(pt1, pt2):
-    ''' compute dot product between two 3d point '''
-    return pt1.x * pt2.x + pt1.y * pt2.y + pt1.z * pt2.z
+def angle_3points(p0, p1, p2):
+    ''' compute the angle in radians between two 3D points
+
+    Calculated as the angle between p0-p1 and p0-p2.
+
+    Args:
+        p0, p1, p2:  indexable objects with
+        indices 0, 1, 2 corresponding to 3D cartesian coordinates.
+
+    Returns:
+        Angle in radians between (p0-p1) and (p0-p2)
+    '''
+    vec1 = vector(p0, p1)
+    vec2 = vector(p0, p2)
+    return acos(np.dot(vec1, vec2) /
+                (np.linalg.norm(vec1) * np.linalg.norm(vec2)))
 
 
-def angle_3points(parent, point1, point2):
-    ''' compute angle in radian of two Point'''
-    vec1 = vector(parent, point1)
-    vec2 = vector(parent, point2)
-    nvec1 = norm(vec1)
-    nvec2 = norm(vec2)
-    val = acos(dot(vec1, vec2) / (nvec1 * nvec2))
-    return val
-
-
-def polygon_diameter(listPoints):
-    ''' Compute the maximun distance between two points of list of Point'''
-    d = max(point_dist(p0, p1) for (p0, p1) in itertools.combinations(listPoints, 2))
-    return d
+def polygon_diameter(points):
+    ''' Compute the maximun euclidian distance between any two points
+    in a list of points
+    '''
+    return max(point_dist(p0, p1) for (p0, p1) in combinations(points, 2))
