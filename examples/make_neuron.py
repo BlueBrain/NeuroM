28df3804c0773d3eb935add3004e3486ac1205e6
Juan Palacios <juan.palacios@epfl.ch>
2015-04-30 11:47:05 +0200

    Extend neuron making example
    * Provide iterator transformations for viewing tree and soma elements
    as points where both contain raw data.
    
    Example:
    
    nrn = make_neuron(....)
    print 'Neuron soma points', [p for p in nrn.soma.iter_point()]
    print 'Print neuron leaves as points'
    for nt in nrn.neurite_trees:
        for p in point_iter(tree.iter_leaf(tt)): print p
    
    Change-Id: Ib14b4e2488f8be9e0556ce6eee24b7ca7d0b822c

--- examples/make_neuron.py
+++ examples/make_neuron.py
@@ -2,6 +2,7 @@
 
 An example of how to build an object representing a neuron from an SWC file
 '''
+from itertools import imap
 from neurom.io.readers import load_data
 from neurom.io.utils import make_tree
 from neurom.io.utils import get_soma_ids
@@ -9,6 +10,15 @@ from neurom.io.utils import get_initial_segment_ids
 from neurom.core import tree
 from neurom.core.dataformat import COLS
 from neurom.core.point import point_from_row
+
+
+def point_iter(iterator):
+    '''Transform tree iterator into a point iterator
+
+    Args:
+        iterator: tree iterator for a tree holding raw data rows.
+    '''
+    return imap(point_from_row, tree.val_iter(iterator))
 
 
 class SOMA_TYPE(object):
@@ -30,27 +40,48 @@ class SOMA_TYPE(object):
                 2: SOMA_TYPE.INVALID}.get(npoints, SOMA_TYPE.C)
 
 
-class SomaA(object):
+class BaseSoma(object):
+    '''Base class for a soma.
+
+    Holds a list of raw data rows corresponding to soma points
+    and provides iterator access to them.
+    '''
+    def __init__(self, points):
+        self._points = points
+
+    def iter(self):
+        '''Iterator to soma contents'''
+        return iter(self._points)
+
+    def iter_point(self):
+        '''Point iterator so soma contents
+
+        Returns neurom.core.point.Point objects.
+        '''
+        return imap(point_from_row, self._points)
+
+
+class SomaA(BaseSoma):
     '''Type A soma'''
     def __init__(self, points):
+        super(SomaA, self).__init__(points)
         _point = point_from_row(points[0])
-        self.points = points
         self.center = _point[:3]
         self.radius = _point.r
 
 
-class SomaB(object):
+class SomaB(BaseSoma):
     '''Type B soma'''
     def __init__(self, points):
-        self.points = points
+        super(SomaB, self).__init__(points)
         self.center = None
         self.radius = None
 
 
-class SomaC(object):
+class SomaC(BaseSoma):
     '''Type C soma'''
     def __init__(self, points):
-        self.points = points
+        super(SomaC, self).__init__(points)
         self.center = None
         self.radius = None
 
@@ -91,21 +122,29 @@ if __name__ == '__main__':
     soma_pts = [rd.get_row(si) for si in get_soma_ids(rd)]
 
     for tr in trees:
-        for p in tree.val_iter(tree.iter_preorder(tr)):
-            print point_from_row(p)
+        for p in point_iter(tree.iter_preorder(tr)):
+            print p
 
     print 'Initial segment IDs:', init_seg_ids
 
     nrn = neuron(soma_pts, trees)
 
-    print 'Neuron soma points', nrn.soma.points
+    print 'Neuron soma raw data', [r for r in nrn.soma.iter()]
+    print 'Neuron soma points', [p for p in nrn.soma.iter_point()]
+
     print 'Neuron tree init points, types'
     for tt in nrn.neurite_trees:
         print tt.value[COLS.ID], tt.value[COLS.TYPE]
 
     print 'Making neuron 2'
     nrn2 = make_neuron(rd)
-    print 'Neuron 2 soma points', nrn2.soma.points
+    print 'Neuron 2 soma points', [r for r in nrn2.soma.iter()]
+    print 'Neuron 2 soma points', [p for p in nrn2.soma.iter_point()]
     print 'Neuron 2 tree init points, types'
     for tt in nrn2.neurite_trees:
         print tt.value[COLS.ID], tt.value[COLS.TYPE]
+
+    print 'Print neuron leaves as points'
+    for tt in nrn2.neurite_trees:
+        for p in point_iter(tree.iter_leaf(tt)):
+            print p
