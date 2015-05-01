1fdeddccae19b65a03f9d30a9822e4754d0221d8
Lida Kanari <lida.kanari@epfl.ch>
2015-05-01 21:29:50 +0200

    Added basic find tree type functionality.
    Change-Id: I6e2e5f4e8ee435eb73eae5a683908d5736cc0297

--- neurom/analysis/morphtree.py
+++ neurom/analysis/morphtree.py
@@ -1,20 +1,22 @@
-'''Tree morphometrics functions '''
-from neurom.core.tree import iter_segment
+'''Basic functions used for tree analysis'''
+from neurom.core import tree as tr
 from neurom.analysis.morphmath import point_dist
 from neurom.core.point import point_from_row
 from neurom.core.point import Point
+from neurom.core.dataformat import COLS
+import numpy as np
 
 
 def get_segment_lengths(tree):
     ''' return a list of segments length inside tree
     '''
-    return [point_dist(point_from_row(s[0]), point_from_row(s[1])) for s in iter_segment(tree)]
+    return [point_dist(point_from_row(s[0]), point_from_row(s[1])) for s in tr.iter_segment(tree)]
 
 
 def get_segment_diameters(tree):
     ''' return a list of segments diameter inside tree
     '''
-    return [point_from_row(s[0]).r + point_from_row(s[1]).r for s in iter_segment(tree)]
+    return [point_from_row(s[0]).r + point_from_row(s[1]).r for s in tr.iter_segment(tree)]
 
 
 def get_segment_radialdists(position, tree):
@@ -23,4 +25,42 @@ def get_segment_radialdists(position, tree):
     return [point_dist(pos, Point((point_from_row(s[0]).x + point_from_row(s[1]).x) / 2.0,
                                   (point_from_row(s[0]).y + point_from_row(s[1]).y) / 2.0,
                                   (point_from_row(s[0]).z + point_from_row(s[1]).z) / 2.0,
-                                  0.0, 0.0)) for s in iter_segment(tree)]
+                                  0.0, 0.0)) for s in tr.iter_segment(tree)]
+
+
+def find_tree_type(tree):
+
+    """
+    Calculates the 'mean' type of the tree.
+    Accepted tree types are:
+    'undefined', 'axon', 'basal', 'apical'
+    The 'mean' tree type is defined as the type
+    that is shared between at least 51% of the tree's points.
+    Returns a tree with a tree.type defined.
+    """
+
+    types = []
+
+    tree_types = ['undefined', 'soma', 'axon', 'basal', 'apical']
+
+    for raw_point in tr.iter_preorder(tree):
+
+        types.append(raw_point.value[COLS.TYPE])
+
+    tree.type = tree_types[int(np.median(types))]
+
+    return tree
+
+
+def get_tree_type(tree):
+
+    """
+    If tree does not have a tree type,
+    calculates the tree type and returns it.
+    If tree has a tree type returns its precomputed type.
+    """
+
+    if not hasattr(tree, 'type'):
+        tree = find_tree_type(tree)
+
+    return tree.type
--- neurom/analysis/tests/test_morphtree.py
+++ neurom/analysis/tests/test_morphtree.py
@@ -1,8 +1,20 @@
 from nose import tools as nt
+import os
 from neurom.core.tree import Tree
+from neurom.io.utils import make_neuron
+from neurom.io.readers import load_data
 from neurom.analysis.morphtree import get_segment_lengths
 from neurom.analysis.morphtree import get_segment_diameters
 from neurom.analysis.morphtree import get_segment_radialdists
+from neurom.analysis.morphtree import find_tree_type
+from neurom.analysis.morphtree import get_tree_type
+
+DATA_PATH = './test_data'
+SWC_PATH = os.path.join(DATA_PATH, 'swc/')
+
+data    = load_data(SWC_PATH + 'Neuron.swc')
+neuron0 = make_neuron(data)
+tree_types = ['axon', 'basal', 'basal', 'apical']
 
 def form_neuron_tree():
     p = [1, 1, 0.0, 0.0, 0.0, 1.0, 2]
@@ -28,14 +40,13 @@ def form_simple_tree():
     T3 = T2.add_child(Tree([1, 1, 0.0, 6.0, 0.0, 1.0, 1]))
     T4 = T3.add_child(Tree([1, 1, 0.0, 8.0, 0.0, 1.0, 1]))
     return T
-    
-    
+
 
 def test_segmentlengths():
 
     T = form_neuron_tree()
 
-    lg=get_segment_lengths(T)
+    lg = get_segment_lengths(T)
 
     nt.assert_equal(lg, [1.0, 1.0, 2.0, 1.0, 2.0, 1.0, 1.0, 2.0, 1.0, 1.0])
 
@@ -48,6 +59,7 @@ def test_segmentdiameters():
 
     nt.assert_equal(dia, [2.0, 2.0, 3.0, 4.0, 3.0, 1.75, 1.5, 3.0, 1.75, 1.5])
 
+
 def test_segmentradialdist():
     T = form_simple_tree()
 
@@ -56,4 +68,20 @@ def test_segmentradialdist():
     rd = get_segment_radialdists(p,T)
 
     #nt.assert_equal(rd, [1.0, 3.0, 5.0, 7.0])
-   
+
+
+def test_find_tree_type():
+    for en_tree, test_tree in enumerate(neuron0.neurite_trees):
+        find_tree_type(test_tree)
+        nt.ok_(test_tree.type == tree_types[en_tree])
+
+
+def test_get_tree_type():
+    for en_tree, test_tree in enumerate(neuron0.neurite_trees):
+        if hasattr(test_tree, 'type'):
+            del test_tree.type
+        # tree.type should be computed here.
+        nt.ok_(get_tree_type(test_tree) == tree_types[en_tree]) 
+        test_tree = find_tree_type(test_tree)
+        # tree.type should already exists here, from previous action.
+        nt.ok_(get_tree_type(test_tree) == tree_types[en_tree]) 
