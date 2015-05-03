d80d8dacf9eccb76674f227758575439031dbddb
Juan Palacios <juan.palacios@epfl.ch>
2015-05-03 15:01:05 +0200

    Add meander angles example
    * examples/meander_angles.py
    
    Note: This detects zero length segments in test_data/swc/Neuron.swc.
          These segments are typically the first ones in each tree.
    
    Change-Id: I664474cc99adb56fd047f94d77bed27c17761fa2

---
+++ examples/meander_angles.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2015, Ecole Polytechnique Federal de Lausanne, Blue Brain Project
+# All rights reserved.
+#
+# This file is part of NeuroM <https://github.com/BlueBrain/NeuroM>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     1. Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#     2. Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in the
+#        documentation and/or other materials provided with the distribution.
+#     3. Neither the name of the copyright holder nor the names of
+#        its contributors may be used to endorse or promote products
+#        derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+'''Calculate inter-segment angles'''
+
+from neurom.core import tree
+from neurom.core.dataformat import COLS
+from neurom.io.readers import load_data
+from neurom.io.utils import make_neuron
+from neurom.analysis.morphmath import angle_3points
+
+
+def fail_info(p0, p1, p2):
+    '''Print info on failed angle calculation points'''
+    print 'WARNING: angle calculation failed for points:\n{0}\n{1}\n{2}\n'.format(p0, p1, p2)
+
+
+if __name__ == '__main__':
+
+    filename = 'test_data/swc/Neuron.swc'
+    rd = load_data(filename)
+    nrn = make_neuron(rd)
+
+    for tt in nrn.neurite_trees:
+        print 'Tree ID: {0}, type: {1}'.format(tt.value[COLS.ID], tt.value[COLS.TYPE])
+        for trp in tree.iter_triplet(tt):
+            points = [n.value for n in trp]
+            try:
+                print 'Angle', angle_3points(points[1], points[0], points[2])
+            except ArithmeticError:
+                fail_info(points[1], points[0], points[2])
