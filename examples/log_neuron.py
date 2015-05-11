84e2f8c798893be5185ae16f6f434737697d7956
Juan Palacios <juan.palacios@epfl.ch>
2015-05-11 15:06:08 +0200

    examples/log_neuron.py: revert to plain logging file handler.
    Change-Id: I07acab3495a6abb0e72b6f610a708991fc761a34

--- examples/log_neuron.py
+++ examples/log_neuron.py
@@ -56,7 +56,7 @@ LOG.addHandler(sh)
 
 def file_handler(logfile, level=logging.WARNING, fmt=None):
     '''create file handler which logs INFO messages'''
-    fh = logging.handlers.RotatingFileHandler(logfile, backupCount=8)
+    fh = logging.FileHandler(logfile)
     fh.setLevel(level)
     if fmt is not None:
         fh.setFormatter(fmt)
