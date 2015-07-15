6ba66d2c5ca7df863b7eb73c668b2d1dc1364c1f
Juan Palacios <juan.palacios@epfl.ch>
2015-07-15 11:56:44 +0200

    Improve raw data checker example.
    Made into executable script that takes either a file
    or a directory as command line argument. For more information,
    use the help:
    
    $ examples/data_checks.py --help
    
    Change-Id: I901cd6b6eaa50001508be998e46b21952056c21e

--- examples/data_checks.py
+++ examples/data_checks.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Copyright (c) 2015, Ecole Polytechnique Federale de Lausanne, Blue Brain Project
 # All rights reserved.
 #
@@ -27,83 +29,68 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 '''Examples of basic data checks'''
-import numpy as np
 from neurom.io.readers import load_data
-from neurom.core.dataformat import ROOT_ID
-from neurom.core.dataformat import COLS
-from neurom.core.dataformat import POINT_TYPE
+from neurom.io import check as io_chk
+import argparse
+import os
 
 
-def has_sequential_ids(raw_data):
-    '''Check that IDs are increasing and consecutive
+EXAMPLE_TEXT = '''
+Examples
+--------
+data_checks.py --help               # print this help
+data_checks.py some/path/neuron.h5  # Process an HDF5 file
+data_checks.py some/path/neuron.swc # Process an SWC file
+data_checks.py some/path/           # Process all HDF5 and SWC files found in directory
+'''
 
-    returns tuple (bool, list of IDs that are not consecutive
-    with their predecessor)
+
+def parse_args():
+    '''Parse command line arguments'''
+    parser = argparse.ArgumentParser(description='NeuroM Data Checker',
+                                     formatter_class=argparse.RawTextHelpFormatter,
+                                     epilog=EXAMPLE_TEXT)
+    parser.add_argument('datapath',
+                        help='Path to morphology data file or directory')
+
+    return parser.parse_args()
+
+
+def get_morph_files(directory):
+    '''Get a list of all morphology files in directory dir
+
+    Returns files with extensions '.swc' or '.h5' (case insensitive)
     '''
-    ids = raw_data.get_col(COLS.ID)
-    steps = [int(j) for (i, j) in zip(ids, ids[1:]) if int(j - i) != 1]
-    return len(steps) == 0, steps
-
-
-def has_soma(raw_data):
-    '''Checks if the TYPE column of raw data block has
-    an element of type soma'''
-    return POINT_TYPE.SOMA in raw_data.get_col(COLS.TYPE)
-
-
-def finite_radius_neurites(raw_data):
-    '''Check that points with neurite type have a finite radius
-
-    return: tuple of (bool, [IDs of neurite points with zero radius])
-    '''
-    bad_points = list()
-    for row in raw_data.iter_row():
-        if row[COLS.TYPE] in POINT_TYPE.NEURITES and row[COLS.R] == 0.0:
-            bad_points.append(int(row[COLS.ID]))
-
-    return len(bad_points) == 0, bad_points
-
-
-def has_finite_length_segments(raw_data):
-    '''Check that all segments have a finite length
-
-    return: tuple of (bool, [(pid, id) for zero-length segments])
-    '''
-    bad_segments = list()
-    for row in raw_data.iter_row():
-        idx = int(row[COLS.ID])
-        pid = int(row[COLS.P])
-        if pid != ROOT_ID:
-            if np.all(row[COLS.X: COLS.R] == raw_data.get_row(pid)[COLS.X: COLS.R]):
-                bad_segments.append((pid, idx))
-    return len(bad_segments) == 0, bad_segments
+    lsdir = [os.path.join(directory, m) for m in os.listdir(directory)]
+    return [m for m in lsdir
+            if os.path.isfile(m) and
+            os.path.splitext(m)[1].lower() in ('.swc', '.h5')]
 
 
 if __name__ == '__main__':
 
-    files = ['test_data/swc/Neuron.swc',
-             'test_data/swc/Single_apical_no_soma.swc',
-             'test_data/swc/Single_apical.swc',
-             'test_data/swc/Single_basal.swc',
-             'test_data/swc/Single_axon.swc',
-             'test_data/swc/Neuron_zero_radius.swc',
-             'test_data/swc/sequential_trunk_off_1_16pt.swc',
-             'test_data/swc/non_sequential_trunk_off_1_16pt.swc']
+    args = parse_args()
+    data_path = args.datapath
+    if os.path.isfile(data_path):
+        files = [data_path]
+    elif os.path.isdir(data_path):
+        print 'Checking files in directory', data_path
+        files = get_morph_files(data_path)
 
     for f in files:
         rd = load_data(f)
-        print 'Check file %s...' % f
-        print 'Has soma? %s' % has_soma(rd)
-        fr = finite_radius_neurites(rd)
+        print '\nCheck file %s...' % f
+        print 'Has soma? %s' % io_chk.has_soma(rd)
+        fr = io_chk.has_all_finite_radius_neurites(rd)
         print 'All neurites have finite radius? %s' % fr[0]
         if not fr[0]:
             print 'Points with zero radius detected:', fr[1]
-        ci = has_sequential_ids(rd)
+        ci = io_chk.has_sequential_ids(rd)
         print 'Consecutive indices? %s' % ci[0]
         if not ci[0]:
             print'Non consecutive IDs detected:', ci[1]
         else:
-            fs = has_finite_length_segments(rd)
+            fs = io_chk.has_all_finite_length_segments(rd)
             print 'Finite length segments? %s' % fs[0]
             if not fs[0]:
-                print 'Segments with zero length detected:', fs[1]
+                print '\tSegments with zero length detected:', fs[1]
