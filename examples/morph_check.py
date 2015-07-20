482d0e2b349f90927d7fcc15d0074de30a4709c9
Juan Palacios <juan.palacios@epfl.ch>
2015-07-20 08:51:50 +0200

    Implement some basic logging functionality in morphology checker example
    * Flags -d or --debug to enable debug information. At the moment, this
      gives a list of "bad" IDs when relevant checks fail. Default is "off".
    * Flags -l or --log with the name of a log file. By default, logs go to
      standard output.
    
    Change-Id: I3373f6637274defd1bcb65edb925050ac7b38379

--- examples/morph_check.py
+++ examples/morph_check.py
@@ -34,6 +34,7 @@ from neurom.check import morphology as io_chk
 from neurom.exceptions import SomaError
 from neurom.exceptions import NonConsecutiveIDsError
 import argparse
+import logging
 import os
 
 DESCRIPTION = '''
@@ -69,6 +70,26 @@ morph_check.py some/path/neuron.swc # Process an SWC file
 morph_check.py some/path/           # Process all HDF5 and SWC files found in directory
 '''
 
+L = logging.getLogger(__name__)
+
+
+def _setup_logging(debug, log_file):
+    """ Set up logger """
+
+    fmt = logging.Formatter('%(levelname)s: %(message)s')
+
+    level = logging.DEBUG if debug else logging.INFO
+    logging.basicConfig(level=level)
+    log = logging.getLogger()
+    log.handlers[0].setFormatter(fmt)
+
+    if log_file:
+        handler = logging.FileHandler(log_file)
+        handler.setFormatter(fmt)
+        log = logging.getLogger()
+        log.addHandler(handler)
+        log.setLevel(logging.DEBUG)
+
 
 def parse_args():
     '''Parse command line arguments'''
@@ -77,6 +98,13 @@ def parse_args():
                                      epilog=EPILOG)
     parser.add_argument('datapath',
                         help='Path to morphology data file or directory')
+
+    parser.add_argument('-d', '--debug',
+                        action='store_true',
+                        help="Log DEBUG information")
+
+    parser.add_argument('-l', '--log', dest='log_file',
+                        default="", help="File to log to")
 
     return parser.parse_args()
 
@@ -94,39 +122,51 @@ def get_morph_files(directory):
 
 def test_file(f):
     '''Run tests on a morphology file'''
-    print '\nCheck file %s...' % f
+
+    LOG_LEVELS = {False: logging.ERROR, True: logging.INFO}
+    L.info('=' * 32)
+    L.info('Check file %s...', f)
+
     try:
         nrn = load_neuron(f)
-        print 'Has valid soma? True'
+        L.info('Has valid soma? True')
     except SomaError:
-        print 'Has valid soma? False'
-        print 'Cannot continue without a soma... Aborting'
+        L.error('Has valid soma? False')
+        L.warning('Cannot continue without a soma... Aborting')
         return
 
-    print 'Has axon? %s' % io_chk.has_axon(nrn)
-    print 'Has apical dendrite? %s' % io_chk.has_apical_dendrite(nrn)
-    print 'Has basal dendrite? %s' % io_chk.has_basal_dendrite(nrn)
+    ok = io_chk.has_axon(nrn)
+    L.log(LOG_LEVELS[ok], 'Has Axon? %s', ok)
 
-    fr = io_chk.all_nonzero_neurite_radii(nrn)
-    print 'All neurites have non-zero radius? %s' % fr[0]
-    if not fr[0]:
-        print '%s points with zero radius detected: %s' % (len(fr[1]), fr[1])
+    ok = io_chk.has_apical_dendrite(nrn)
+    L.log(LOG_LEVELS[ok], 'Has Apical Dendrite? %s', ok)
 
-    fs = io_chk.all_nonzero_segment_lengths(nrn)
-    print 'All segments have non-zero length? %s' % fs[0]
-    if not fs[0]:
-        print '\tSegments with zero length detected:', fs[1]
+    ok = io_chk.has_basal_dendrite(nrn)
+    L.log(LOG_LEVELS[ok], 'Has Basal Dendrite? %s', ok)
 
-    fs = io_chk.all_nonzero_section_lengths(nrn)
-    print 'All sections have non-zero length? %s' % fs[0]
-    if not fs[0]:
-        print '\tSections with zero length detected:', fs[1]
+    ok, pts = io_chk.all_nonzero_neurite_radii(nrn)
+    L.log(LOG_LEVELS[ok], 'All neurites have non-zero radius? %s', ok)
+    if not ok:
+        L.debug('%s points with zero radius detected: %s', len(pts), pts)
 
+    ok, pts = io_chk.all_nonzero_segment_lengths(nrn)
+    L.log(LOG_LEVELS[ok], 'All segments have non-zero length? %s', ok)
+    if not ok:
+        L.debug('%d segments with zero length detected: %s', len(pts), pts)
+
+    ok, pts = io_chk.all_nonzero_section_lengths(nrn)
+    L.log(LOG_LEVELS[ok], 'All sections have non-zero length? %s', ok)
+    if not ok:
+        L.debug('%d sections with zero length detected: %s', len(pts), pts)
+
+    L.info('=' * 32)
 
 if __name__ == '__main__':
 
     args = parse_args()
+    _setup_logging(args.debug, args.log_file)
     data_path = args.datapath
+
     if os.path.isfile(data_path):
         files = [data_path]
     elif os.path.isdir(data_path):
