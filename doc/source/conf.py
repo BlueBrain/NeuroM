ef2a8b9bde5c9af98f3d31c1ec7b9c33f36f12b6
Juan Palacios <juan.palacios@epfl.ch>
2015-08-03 14:35:54 +0200

    Add copyright and last updated on information to doc page footers.
    Change-Id: Ie6201ed96347e00f28ec47094c755bc5ecdad92a

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -82,7 +82,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'NeuroM'
-author = u'HBP Algorithm Development Section'
+author = u'BBP Algorithm Development Section'
+copyright = u'2015, Ecole Polytechnique Federale de Lausanne, Blue Brain Project'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -210,7 +211,7 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
