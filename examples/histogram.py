c6a96d0687d9077f1ef2fe3af69e553fe7e2029c
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2015-10-07 16:52:42 +0200

    docstr corrections

--- examples/histogram.py
+++ examples/histogram.py
@@ -42,15 +42,11 @@ def histogram(neurons, feature, new_fig=True, subplot=False, **kwargs):
 
     Parameters
     ----------
-    feature : str
-    The feature of interest.
-
-    subplot : int
-    The subplot identifier e. 223 for the third subplot in a 2x2
-    subplot grid.
-
     neurons : list
         List of Neurons. Single neurons must be encapsulated in a list.
+
+    feature : str
+    The feature of interest.
 
     Options
     -------
@@ -63,7 +59,6 @@ def histogram(neurons, feature, new_fig=True, subplot=False, **kwargs):
     subplot : bool
         Default is False, which returns a matplotlib figure object. If True,
         returns a matplotlib axis object, for use as a subplot.
-    output_format : str
 
     Returns
     -------
@@ -74,25 +69,6 @@ def histogram(neurons, feature, new_fig=True, subplot=False, **kwargs):
         generate the figure. The final item is text used in report generation
         as a figure legend. This text needs to be manually entered in each
         figure file.
-
-    Notes
-    -----
-    Any notes would go here.
-
-    References
-    ----------
-    [1] Ascoli GA, Krichmar JL (2000) L-neuron: A modeling tool for the
-    efficient generation and parsimonious description of dendritic morphology.
-    Neurocomputing 32-33:1003-1011
-
-    Examples
-    --------
-    This is an optional section giving examples of how to use the function.
-    It should be formatted as follows:
-
-    >>> function_output = my_function(param1, param2)
-    function_output
-
     '''
 
     bins = kwargs.get('bins', 100)
