4f4fe16eaf8adeef9ebe144dcf5e3a0056d15c48
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2015-10-07 16:50:20 +0200

    boxplot example

---
+++ examples/boxplot.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# Copyright (c) 2015, Ecole Polytechnique Federale de Lausanne, Blue Brain Project
+# All rights reserved.
+#
+# This file is part of NeuroM <https://github.com/BlueBrain/NeuroM>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     1. Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#     2. Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in the
+#        documentation and/or other materials provided with the distribution.
+#     3. Neither the name of the copyright holder nor the names of
+#        its contributors may be used to endorse or promote products
+#        derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+''' Box Plot function for multiple neurons
+'''
+
+from neurom.view import common
+
+
+def boxplot(neurons, feature, new_fig=True, subplot=False):
+    '''
+    Plot a histogram of the selected feature for the population of neurons.
+    Plots x-axis versus y-axis on a scatter|histogram|binned values plot.
+
+    More information about the plot and how it works.
+
+    Parameters
+    ----------
+    neurons : list
+        List of Neurons. Single neurons must be encapsulated in a list.
+
+    feature : str
+    The feature of interest.
+
+    Options
+    -------
+
+    subplot : bool
+        Default is False, which returns a matplotlib figure object. If True,
+        returns a matplotlib axis object, for use as a subplot.
+
+    '''
+    feature_values = [getattr(neu, 'get_' + feature)() for neu in neurons]
+
+    _, ax = common.get_figure(new_fig=new_fig, subplot=subplot)
+
+    ax.boxplot(feature_values)
+
+    x_labels = ['neuron_id' for _ in neurons]
+
+    ax.set_xticklabels(x_labels)
