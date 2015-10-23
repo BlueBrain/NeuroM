76ba996ac3fad86dd70720c61ed3c2a60a34323f
Juan Palacios <juan.palacios@epfl.ch>
2015-10-23 14:25:20 +0200

    bugfix: ezy.Neuron.get_trunk_radii and get_trunk_lengths
    Return type was list, but scipy analysis assumes numpy arrays.
    Write out trunk_radius in synthesizer JSON.

--- examples/synthesis_json.py
+++ examples/synthesis_json.py
@@ -171,6 +171,12 @@ if __name__ == '__main__':
          {"neurite_type": ezy.TreeType.apical_dendrite}],
         ["segment_length", "segment_length", "axon", 0, None,
          {"neurite_type": ezy.TreeType.axon}],
+        ["trunk_radius", "trunk_radius", "basal_dendrite", 0, None,
+         {"neurite_type": ezy.TreeType.basal_dendrite}],
+        ["trunk_radius", "trunk_radius", "apical_dendrite", 0, None,
+         {"neurite_type": ezy.TreeType.apical_dendrite}],
+        ["trunk_radius", "trunk_radius", "axon", 0, None,
+         {"neurite_type": ezy.TreeType.axon}],
     ]
 
     for d in data_dirs:
