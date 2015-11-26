2fb1817ef8935a9af42cd4a5227c2b87e48990e7
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2015-11-26 09:10:55 +0100

    corrected pep8

--- neurom/analysis/dendrogram.py
+++ neurom/analysis/dendrogram.py
@@ -1,6 +1,34 @@
+# Copyright (c) 2015, Ecole Polytechnique Federale de Lausanne, Blue Brain Project
+# All rights reserved.
+#
+# This file is part of NeuroM <https://github.com/BlueBrain/NeuroM>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     1. Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#     2. Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in the
+#        documentation and/or other materials provided with the distribution.
+#     3. Neither the name of the copyright holder nor the names of
+#        its contributors may be used to endorse or promote products
+#        derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+'''Dendrogram helper functions and class'''
 
-from neurom.core.tree import Tree, val_iter
+from neurom.core.tree import Tree
 from neurom.core.neuron import Neuron
 from neurom.analysis.morphtree import n_segments, n_bifurcations, n_terminations
 from neurom.analysis.morphmath import segment_length
@@ -41,7 +69,8 @@ def _n_rectangles(obj):
 
 
 def displace(rectangles, t):
-    
+    '''Displace the collection of rectangles
+    '''
     n, m, _ = rectangles.shape
 
     for i in xrange(n):
@@ -81,16 +110,21 @@ def _spacingx(node, max_dims, offsets, spacing):
 
     return offsets[0] - x_spacing / 2.
 
-def _update_offsets(start_x, spacing, terminations, offsets, length):
 
+def _update_offsets(start_x, spacing, terminations, offsets, length):
+    '''Update the offsets
+    '''
     return (start_x + spacing[0] * terminations / 2.,
             offsets[1] + spacing[1] * 2. + length)
 
 
 class Dendrogram(object):
-
+    '''Dendrogram
+    '''
 
     def __init__(self, obj):
+        '''Create dendrogram
+        '''
 
         # input object, tree, or neuron
         self._obj = obj
@@ -119,6 +153,8 @@ class Dendrogram(object):
         print "nlines : ", _n_rectangles(self._obj)
 
     def run(self):
+        '''Generate dendrogram
+        '''
 
         sys.setrecursionlimit(100000)
 
@@ -130,7 +166,7 @@ class Dendrogram(object):
 
         if isinstance(self._obj, Tree):
 
-            self._generate_dendro(self._obj, self._rectangles, spacing)
+            self._generate_dendro(self._obj, spacing, offsets)
 
             self._trees = [self._rectangles]
 
@@ -138,12 +174,12 @@ class Dendrogram(object):
 
             n_previous = 0
 
-            for neurite in self._obj.neurites:          
+            for neurite in self._obj.neurites:
 
-                self._generate_dendro(neurite, self._rectangles, spacing, offsets)
+                self._generate_dendro(neurite, spacing, offsets)
 
                 # store in trees the sliced array of lines for each neurite
-                self._trees.append(self._rectangles[n_previous : self._n])
+                self._trees.append(self._rectangles[n_previous: self._n])
 
                 # store the max dims per neurite for view positioning
                 self._dims.append(self._max_dims)
@@ -154,13 +190,13 @@ class Dendrogram(object):
                 # keep track of the next tree start index in list
                 n_previous = self._n
 
-
     @property
-    def data():
-        return _rectangles
+    def data(self):
+        ''' data
+        '''
+        return self._rectangles
 
-
-    def _generate_dendro(self, current_node, lines, spacing, offsets):
+    def _generate_dendro(self, current_node, spacing, offsets):
         '''Recursive function for dendrogram line computations
         '''
         max_dims = self._max_dims
@@ -188,10 +224,10 @@ class Dendrogram(object):
             new_offsets = _update_offsets(start_x, spacing, terminations, offsets, ln)
 
             # create and store vertical segment
-            lines[self._n] = _vertical_segment(offsets, new_offsets, spacing, radii)
+            self._rectangles[self._n] = _vertical_segment(offsets, new_offsets, spacing, radii)
 
             # assign segment id to color array
-            #colors[n[0]] = child.value[4]
+            # colors[n[0]] = child.value[4]
             self._n += 1
 
             if offsets[1] + spacing[1] * 2 + ln > max_dims[1]:
@@ -199,38 +235,48 @@ class Dendrogram(object):
 
             self._max_dims = max_dims
             # recursive call to self.
-            self._generate_dendro(child, lines, spacing, new_offsets)
+            self._generate_dendro(child, spacing, new_offsets)
 
             # update the starting position for the next child
             start_x += terminations * spacing[0]
 
-            # write the horizontal lines only for bifurcations, where the are actual horizontal lines
-            # and not zero ones
+            # write the horizontal lines only for bifurcations, where the are actual horizontal
+            # lines and not zero ones
             if offsets[0] != new_offsets[0]:
 
                 # horizontal segment
-                lines[self._n] = _horizontal_segment(offsets, new_offsets, spacing, 0.)
-                #colors[self._n] = current_node.value[4]
+                self._rectangles[self._n] = _horizontal_segment(offsets, new_offsets, spacing, 0.)
+                # colors[self._n] = current_node.value[4]
                 self._n += 1
 
-
     def view(self, new_fig=True, subplot=None, **kwargs):
+        '''
+        Dendrogram Viewer
+        '''
+
+        def _format_str(string):
+            ''' string formatting
+            '''
+            return string.replace('TreeType.', '').replace('_', ' ').capitalize()
 
         fig, ax = common.get_figure(new_fig=new_fig, subplot=subplot)
 
         displacement = 0.
         colors = set()
+
         for i, group in enumerate(self._trees):
 
             if i > 0:
-                displacement += 0.5 * (self._dims[i-1][0] + self._dims[i][0])
+                displacement += 0.5 * (self._dims[i - 1][0] + self._dims[i][0])
 
             # arrange the trees without overlapping with each other
             displace(group, (displacement, 0.))
 
             # color
             tree_type = self._obj.neurites[i].type
+
             color = common.TREE_COLOR[tree_type]
+
             # generate segment collection
             collection = PolyCollection(group, closed=False, antialiaseds=True,
                                         edgecolors=color, facecolors=color)
@@ -240,21 +286,16 @@ class Dendrogram(object):
 
             # dummy plot for the legend
             if color not in colors:
-                ax.plot((0., 0.), (0., 0.), c=color, label=str(tree_type).replace('TreeType.','').replace('_',' ').capitalize())
+                ax.plot((0., 0.), (0., 0.), c=color, label=_format_str(str(tree_type)))
                 colors.add(color)
 
         ax.autoscale(enable=True, tight=None)
 
-        # dummy plots for color bar labels
-        #for color, label in set(linked_colors):
-        #    
-
         # customization settings
-        #kwargs['xticks'] = []
+        # kwargs['xticks'] = []
         kwargs['title'] = kwargs.get('title', 'Morphology Dendrogram')
         kwargs['xlabel'] = kwargs.get('xlabel', '')
         kwargs['ylabel'] = kwargs.get('ylabel', '')
         kwargs['no_legend'] = False
 
         return common.plot_style(fig=fig, ax=ax, **kwargs)
-
