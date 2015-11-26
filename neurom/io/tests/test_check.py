c526b1f3c027eeab1a4768b3b0f92a6600424349
Juan Palacios <juan.palacios@epfl.ch>
2015-11-26 14:47:46 +0100

    Make load_data function available in neurom.io
    * neurom.io.load_data is alias for neurom.io.readers.load_data
    * code clean-ups

--- neurom/io/tests/test_check.py
+++ neurom/io/tests/test_check.py
@@ -27,12 +27,7 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import os
-import numpy as np
-from neurom.io.readers import load_data
-from neurom.io import check
-from neurom.core.dataformat import ROOT_ID
-from neurom.core.dataformat import COLS
-from neurom.core.dataformat import POINT_TYPE
+from neurom import io
 from nose import tools as nt
 
 
@@ -58,7 +53,7 @@ def test_has_sequential_ids_good_data():
              ]
 
     for f in files:
-        ok, ids = check.has_sequential_ids(load_data(f))
+        ok, ids = io.check.has_sequential_ids(io.load_data(f))
         nt.ok_(ok)
         nt.ok_(len(ids) == 0)
 
@@ -68,7 +63,7 @@ def test_has_sequential_ids_bad_data():
 
     f = os.path.join(SWC_PATH, 'Neuron_missing_ids.swc')
 
-    ok, ids = check.has_sequential_ids(load_data(f))
+    ok, ids = io.check.has_sequential_ids(io.load_data(f))
     nt.ok_(not ok)
     nt.ok_(ids == [6, 217, 428, 639])
 
@@ -83,12 +78,12 @@ def test_has_soma_points_good_data():
     files.append(os.path.join(H5V1_PATH, 'Neuron_2_branch.h5'))
 
     for f in files:
-        nt.ok_(check.has_soma_points(load_data(f)))
+        nt.ok_(io.check.has_soma_points(io.load_data(f)))
 
 
 def test_has_soma_points_bad_data():
     f = os.path.join(SWC_PATH, 'Single_apical_no_soma.swc')
-    nt.ok_(not check.has_soma_points(load_data(f)))
+    nt.ok_(not io.check.has_soma_points(io.load_data(f)))
 
 
 def test_has_finite_radius_neurites_good_data():
@@ -101,14 +96,14 @@ def test_has_finite_radius_neurites_good_data():
     files.append(os.path.join(H5V1_PATH, 'Neuron_2_branch.h5'))
 
     for f in files:
-        ok, ids = check.has_all_finite_radius_neurites(load_data(f))
+        ok, ids = io.check.has_all_finite_radius_neurites(io.load_data(f))
         nt.ok_(ok)
         nt.ok_(len(ids) == 0)
 
 
 def test_has_finite_radius_neurites_bad_data():
     f = os.path.join(SWC_PATH, 'Neuron_zero_radius.swc')
-    ok, ids = check.has_all_finite_radius_neurites(load_data(f))
+    ok, ids = io.check.has_all_finite_radius_neurites(io.load_data(f))
     nt.ok_(not ok)
     nt.ok_(ids == [194, 210, 246, 304, 493])
 
@@ -120,7 +115,7 @@ def test_has_finite_length_segments_good_data():
                        'sequential_trunk_off_1_16pt.swc',
                        'sequential_trunk_off_42_16pt.swc']]
     for f in files:
-        ok, ids = check.has_all_finite_length_segments(load_data(f))
+        ok, ids = io.check.has_all_finite_length_segments(io.load_data(f))
         nt.ok_(ok)
         nt.ok_(len(ids) == 0)
 
@@ -139,6 +134,6 @@ def test_has_finite_length_segments_bad_data():
                 [(4, 5)]]
 
     for i, f in enumerate(files):
-        ok, ids = check.has_all_finite_length_segments(load_data(f))
+        ok, ids = io.check.has_all_finite_length_segments(io.load_data(f))
         nt.ok_(not ok)
         nt.assert_equal(ids, bad_segs[i])
