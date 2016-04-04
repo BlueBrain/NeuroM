aad373bcd538851d277a51482359797dfe217258
Lida Kanari <lida.kanari@epfl.ch>
2016-04-04 17:15:01 +0200

    Fix pylint errors

--- examples/density_plot.py
+++ examples/density_plot.py
@@ -1,22 +1,59 @@
+# Copyright (c) 2015, Ecole Polytechnique Federale de Lausanne, Blue Brain Project
+# All rights reserved.
+#
+# This file is part of NeuroM <https://github.com/BlueBrain/NeuroM>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     1. Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#     2. Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in the
+#        documentation and/or other materials provided with the distribution.
+#     3. Neither the name of the copyright holder nor the names of
+#        its contributors may be used to endorse or promote products
+#        derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+'''Example for generating density plots'''
+
 from neurom.features import NEURITEFEATURES as nf
 import pylab as plt
 from neurom.view import common
 from neurom.view import view
+import numpy as np
 
 
 def extract_density(population, plane='xy', bins=100):
-
-    horiz = nf['segment_' + plane[0] + '_coordinates'](pop)
-    vert = nf['segment_' + plane[1] + '_coordinates'](pop)
+    '''Extracts the 2d histogram of the center
+       coordinates of segments in the selected plane.
+    '''
+    horiz = nf['segment_' + plane[0] + '_coordinates'](population)
+    vert = nf['segment_' + plane[1] + '_coordinates'](population)
 
     return np.histogram2d(np.array(horiz), np.array(vert),
                           bins=(bins, bins))
 
 
-def plot_density(population, plane='xy', bins=100, new_fig=True, subplot=111,
-                 colorlabel='Nodes per unit area', labelfontsize=16, levels=None,
-                 color_map='Reds', no_colorbar=False, threshold=0.01, **kwargs):
-
+def plot_density(population, # pylint: disable=too-many-arguments, too-many-locals
+                 bins=100, new_fig=True, subplot=111, levels=None, plane='xy',
+                 colorlabel='Nodes per unit area', labelfontsize=16,
+                 color_map='Reds', no_colorbar=False, threshold=0.01,
+                 **kwargs):
+    '''Plots the 2d histogram of the center
+       coordinates of segments in the selected plane.
+    '''
     fig, ax = fig, ax = common.get_figure(new_fig=new_fig, subplot=subplot)
 
     H1, xedges1, yedges1 = extract_density(population, plane=plane, bins=bins)
@@ -42,11 +79,16 @@ def plot_density(population, plane='xy', bins=100, new_fig=True, subplot=111,
     return common.plot_style(fig=fig, ax=ax, **kwargs)
 
 
-def plot_neuron_on_density(population, plane='xy', bins=100, new_fig=True, subplot=111,
-                           colorlabel='Nodes per unit area', labelfontsize=16, levels=None,
-                           color_map='Reds', no_colorbar=False, threshold=0.01, **kwargs):
-
-    fig, ax = view.tree(pop.neurites[0], new_fig=new_fig)
+def plot_neuron_on_density(population, # pylint: disable=too-many-arguments
+                           bins=100, new_fig=True, subplot=111, levels=None, plane='xy',
+                           colorlabel='Nodes per unit area', labelfontsize=16,
+                           color_map='Reds', no_colorbar=False, threshold=0.01,
+                           **kwargs):
+    '''Plots the 2d histogram of the center
+       coordinates of segments in the selected plane
+       and superimposes the view of the first neurite of the collection.
+    '''
+    view.tree(population.neurites[0], new_fig=new_fig)
 
     return plot_density(population, plane=plane, bins=bins, new_fig=False, subplot=subplot,
                         colorlabel=colorlabel, labelfontsize=labelfontsize, levels=levels,
