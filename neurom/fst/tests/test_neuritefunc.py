04df6d4f310991bcc2e87a2649e9a6b4c5b58a8c
Mike Gevaert <michael.gevaert@epfl.ch>
2016-11-04 11:30:18 +0100

    Make swc file non symmetric

--- neurom/fst/tests/test_neuritefunc.py
+++ neurom/fst/tests/test_neuritefunc.py
@@ -192,7 +192,7 @@ def test_terminal_length_per_neurite():
     nrn = nm.load_neuron(os.path.join(SWC_PATH, 'simple.swc'))
     terminal_distances = np.array(_nf.terminal_path_lengths_per_neurite(nrn))
     np.testing.assert_allclose(terminal_distances,
-                               np.array([4 + 5., 4 + 6., 4. + 6., 4. + 5]))
+                               np.array([5 + 5., 5 + 6., 4. + 6., 4. + 5]))
     terminal_distances = np.array(_nf.terminal_path_lengths_per_neurite(
         nrn, neurite_type=nm.AXON))
     np.testing.assert_allclose(terminal_distances,
