b2f790dd2e49167df9ca8fd00b2275411bdb9fff
Benoît Coste <benoit.coste@protonmail.com>
2020-01-24 15:54:34 +0100

    Add test multifurcation

--- neurom/check/neuron_checks.py
+++ neurom/check/neuron_checks.py
@@ -340,6 +340,6 @@ def has_no_narrow_neurite_section(neuron,
 
 def has_multifurcation(neuron):
     '''Check if a section has more than 3 children'''
-    bad_ids = [(section.id, [section.points[-1]]) for section in iter_sections(neuron)
+    bad_ids = [(section.id, section.points[-1]) for section in iter_sections(neuron)
                if len(section.children) > 3]
     return CheckResult(len(bad_ids) == 0, bad_ids)
