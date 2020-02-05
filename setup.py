276a324c57dd661da55658cc1cb8b9c329597cad
Benoît Coste <benoit.coste@epfl.ch>
2020-02-05 15:23:08 +0100

    Re-add psutil
    removed by mistake during merge

--- setup.py
+++ setup.py
@@ -73,7 +73,9 @@ config = {
     },
     'name': 'neurom',
     'extras_require': {
-        'plotly': ['plotly>=3.6.0'],
+        'plotly': ['plotly>=3.6.0',
+                   'psutil>=5.5.1',  # for plotly image saving
+                   ],
     },
     'include_package_data': True,
 
