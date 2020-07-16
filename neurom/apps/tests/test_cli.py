33739243fe1bcd35b568fc8efd343da295afd81f
Benoît Coste <benoit.coste@epfl.ch>
2020-07-16 13:55:38 +0200

    revert changes to test_cli.py made by mistake

--- neurom/apps/tests/test_cli.py
+++ neurom/apps/tests/test_cli.py
@@ -9,29 +9,34 @@ from neurom.apps.cli import cli
 DATA = Path(__file__).parent.parent.parent.parent.resolve() / 'test_data'
 
 
-@patch('neurom.apps.cli.plt.show', MagicMock())
-def test_viewer_matplotlib():
+@patch('neurom.apps.cli.plt.show')
+def test_viewer_matplotlib(mock):
     runner = CliRunner()
     filename = str(DATA / 'swc' / 'simple.swc')
 
     result = runner.invoke(cli, ['view', filename])
     assert_equal(result.exit_code, 0)
+    mock.assert_called_once()
 
+    mock.reset_mock()
     result = runner.invoke(cli, ['view', filename, '--plane', 'xy'])
     assert_equal(result.exit_code, 0)
+    mock.assert_called_once()
 
 
-@patch('neurom.view.plotly.plot', MagicMock())
-def test_viewer_plotly():
-
+@patch('neurom.view.plotly.plot')
+def test_viewer_plotly(mock):
     runner = CliRunner()
     filename = str(DATA / 'swc' / 'simple.swc')
 
     result = runner.invoke(cli, ['view', filename,
                                  '--backend', 'plotly'])
     assert_equal(result.exit_code, 0)
-    
+    mock.assert_called_once()
+
+    mock.reset_mock()
     result = runner.invoke(cli, ['view', filename,
                                  '--backend', 'plotly',
                                  '--plane', 'xy'])
     assert_equal(result.exit_code, 0)
+    mock.assert_called_once()
