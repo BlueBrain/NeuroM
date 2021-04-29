7932801753466f7e2f0cbce6be85dddf4c1a93cf
aleksei sanin <aleksei.sanin@epfl.ch>
2021-04-29 11:58:44 +0200

    clean

--- neurom/core/types.py
+++ neurom/core/types.py
@@ -53,7 +53,6 @@ class NeuriteType(IntEnum):
     undefined = SectionType.undefined
     soma = 31
     all = 32
-    # custom = SectionType.custom5
     custom5 = SectionType.custom5
     custom6 = SectionType.custom6
     custom7 = SectionType.custom7
