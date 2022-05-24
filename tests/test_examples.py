c97f005f248c7f9228c2edc081265a61b8018637
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2022-05-24 09:53:36 +0300

    Run examples in a tempdir to avoid creating files in root

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,3 +1,5 @@
+import os
+import tempfile
 import subprocess
 from pathlib import Path
 
@@ -10,5 +12,15 @@ print(EXAMPLES_DIR)
 
 @pytest.mark.parametrize("filename", EXAMPLES_DIR.glob("*.py"))
 def test_example(filename):
-    result = subprocess.run(["python", filename], capture_output=False)
-    assert result.returncode == 0
+
+    cwd = os.getcwd()
+
+    with tempfile.TemporaryDirectory() as tempdir:
+
+        # change directory to avoid creating files in the root folder
+        os.chdir(tempdir)
+
+        result = subprocess.run(["python", filename], capture_output=False)
+        assert result.returncode == 0
+
+    os.chdir(cwd)
