ad36120486255e302a9eee9b223aeef79037ef1f
Adrien Berchet <adrien.berchet@gmail.com>
2022-09-26 16:12:29 +0200

    Fix: extract_stats could not work on single neurite (#1060)

--- neurom/apps/morph_stats.py
+++ neurom/apps/morph_stats.py
@@ -46,7 +46,7 @@ from morphio import SomaError
 
 import neurom as nm
 from neurom.apps import get_config
-from neurom.core.morphology import Morphology
+from neurom.core.morphology import Morphology, Neurite
 from neurom.exceptions import ConfigError
 from neurom.features import _NEURITE_FEATURES, _MORPHOLOGY_FEATURES, _POPULATION_FEATURES, \
     _get_feature_value_and_func
@@ -185,9 +185,10 @@ def extract_stats(morphs, config):
         The extracted statistics
 
     Note:
-        An example config can be found at:
+        An example config can be found in the `CLI -> neurom stats` page of the documentation.
 
     """
+    # pylint: disable=too-many-nested-blocks
     config = _sanitize_config(config)
 
     neurite_types = [_NEURITE_MAP[t] for t in config.get('neurite_type', _NEURITE_MAP.keys())]
@@ -213,7 +214,8 @@ def extract_stats(morphs, config):
                     )
 
                     for neurite_type in types:
-                        feature_kwargs["neurite_type"] = neurite_type
+                        if not isinstance(morphs, Neurite):
+                            feature_kwargs["neurite_type"] = neurite_type
                         stats[neurite_type.name].update(
                             _get_feature_stats(feature_name, morphs, modes, feature_kwargs)
                         )
