4a5935b30ed17b1af8c9eb4b6a70c38a20d3f197
Adrien Berchet <adrien.berchet@gmail.com>
2022-10-10 10:30:41 +0200

    Reorganize tests

--- tests/features/test_features.py
+++ tests/features/test_features.py
@@ -59,41 +59,40 @@ def _check_nested_type(data):
         assert isinstance(data, (int, float))
 
 
-class TestFeatureTypes:
-    """Test that all features return raw Python types."""
+@pytest.mark.parametrize(
+    "feature_name",
+    [
+        pytest.param(name, id=f"Test type of {name} neurite feature")
+        for name in features._NEURITE_FEATURES
+    ],
+)
+def test_neurite_feature_types(feature_name, NEURITE):
+    """Test neurite features."""
+    res = features._NEURITE_FEATURES.get(feature_name)(NEURITE)
+    _check_nested_type(res)
 
-    @pytest.mark.parametrize(
-        "feature_name",
-        [
-            pytest.param(name, id=f"Test type of {name} neurite feature")
-            for name in features._NEURITE_FEATURES
-        ],
-    )
-    def test_neurite_feature_types(self, feature_name, NEURITE):
-        """Test neurite features."""
-        res = features._NEURITE_FEATURES.get(feature_name)(NEURITE)
-        _check_nested_type(res)
 
-    @pytest.mark.parametrize(
-        "feature_name",
-        [
-            pytest.param(name, id=f"Test type of {name} morphology feature")
-            for name in features._MORPHOLOGY_FEATURES
-        ],
-    )
-    def test_morphology_feature_types(self, feature_name, MORPHOLOGY):
-        """Test morphology features."""
-        res = features._MORPHOLOGY_FEATURES.get(feature_name)(MORPHOLOGY)
-        _check_nested_type(res)
+@pytest.mark.parametrize(
+    "feature_name",
+    [
+        pytest.param(name, id=f"Test type of {name} morphology feature")
+        for name in features._MORPHOLOGY_FEATURES
+    ],
+)
+def test_morphology_feature_types(feature_name, MORPHOLOGY):
+    """Test morphology features."""
+    res = features._MORPHOLOGY_FEATURES.get(feature_name)(MORPHOLOGY)
+    _check_nested_type(res)
 
-    @pytest.mark.parametrize(
-        "feature_name",
-        [
-            pytest.param(name, id=f"Test type of {name} population feature")
-            for name in features._POPULATION_FEATURES
-        ],
-    )
-    def test_population_feature_types(self, feature_name, POP):
-        """Test population features."""
-        res = features._POPULATION_FEATURES.get(feature_name)(POP)
-        _check_nested_type(res)
+
+@pytest.mark.parametrize(
+    "feature_name",
+    [
+        pytest.param(name, id=f"Test type of {name} population feature")
+        for name in features._POPULATION_FEATURES
+    ],
+)
+def test_population_feature_types(feature_name, POP):
+    """Test population features."""
+    res = features._POPULATION_FEATURES.get(feature_name)(POP)
+    _check_nested_type(res)
